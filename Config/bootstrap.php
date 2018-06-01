e2cf1995cd6b94915c87b8f9612b998bc3885c34
Benjam Welker <benjamwelker@gmail.com>
2018-05-31 20:47:36 -0600

    convert file to UTF8

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -531,7 +531,7 @@ CakeLog::config('error', array(
 	function link_urls($text, $twitter = false) {
 		// replace URLs with clickable links
 		// http://daringfireball.net/2010/07/improved_regex_for_matching_urls
-		$text = preg_replace('#\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()[\]{};:\'".,<>?«»“”‘’]))#i', '<a href="$1" rel="nofollow">$1</a>', $text);
+		$text = preg_replace('#\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()[\]{};:\'".,<>?Â«Â»â€œâ€â€˜â€™]))#i', '<a href="$1" rel="nofollow">$1</a>', $text);
 
 		if ($twitter) {
 			// replace any @mentions with links to the users profile
